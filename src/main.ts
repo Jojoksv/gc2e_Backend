ec4df06865406d104c14c9583e3a862502e3e646
JojoKsv <jkossouvi@gmail.com>
2025-01-06 23:28:14 +0000

    setting requests origin cors methods 1

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import { NextFunction, Response } from 'express';
+import { Request, NextFunction, Response } from 'express';
 
 const ENV = process.env.NODE_ENV;
 const front_URL = process.env.FRONTEND_URL;
@@ -23,16 +23,28 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  app.use(function (request: Request, response: Response, next: NextFunction) {
-    response.setHeader('Access-Control-Allow-Origin', 'https://genie-construction-eben-ezer.vercel.app');
-    next();
+  app.enableCors({
+    origin: ['https://genie-construction-eben-ezer.vercel.app'], // Frontend autorisé
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes autorisées
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // En-têtes autorisés
+    credentials: true, // Autoriser les cookies
   });
 
-  app.enableCors({
-    origin: 'https://genie-construction-eben-ezer.vercel.app', // Ton frontend spécifique
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Méthodes autorisées
-    allowedHeaders: 'Content-Type, Authorization, Accept', // En-têtes autorisés
-    credentials: true, // Permet l'envoi et la réception des cookies
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Origin',
+        'https://genie-construction-eben-ezer.vercel.app',
+      );
+      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Accept',
+      );
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.status(200).json({});
+    }
+    next();
   });
 
   await app.listen(process.env.PORT ?? 3000);
