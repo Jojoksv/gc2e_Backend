04398a3af4847335ed70edd7ea34fcee34f1ee65
JojoKsv <jkossouvi@gmail.com>
2025-01-06 15:14:23 +0000

    hello world gc2e

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 const ENV = process.env.NODE_ENV;
-const URL = process.env.FRONTEND_URL;
+const front_URL = process.env.FRONTEND_URL;
 
-const origin = ENV === 'PROD' ? URL : 'https://localhost:5174';
+const origin = ENV === 'PROD' ? front_URL : 'https://localhost:5174';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
