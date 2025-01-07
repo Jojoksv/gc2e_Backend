07bd76640bbadf5aa2e298b451c9f29bf04a50cb
JojoKsv <jkossouvi@gmail.com>
2025-01-07 14:15:40 +0000

    setting cors 3

--- src/main.ts
+++ src/main.ts
@@ -28,36 +28,34 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  // app.enableCors();
-
   app.use((req: Request, res: Response, next: NextFunction) => {
     console.log('Request Headers:', req.headers);
     next();
   });
 
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Origin',
+        'https://genie-construction-eben-ezer.vercel.app',
+      );
+      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Accept',
+      );
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.status(200).json({});
+    }
+    next();
+  });
+
   app.enableCors({
-    origin: ['https://genie-construction-eben-ezer.vercel.app'], // Frontend autorisé
+    origin: ['https://genie-construction-eben-ezer.vercel.app/'], // Frontend autorisé
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes autorisées
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // En-têtes autorisés
     credentials: true, // Autoriser les cookies
   });
-
-  // app.use(function (req: Request, res: Response, next: NextFunction) {
-  //   if (req.method === 'OPTIONS') {
-  //     res.header(
-  //       'Access-Control-Allow-Origin',
-  //       'https://genie-construction-eben-ezer.vercel.app',
-  //     );
-  //     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  //     res.header(
-  //       'Access-Control-Allow-Headers',
-  //       'Content-Type, Authorization, Accept',
-  //     );
-  //     res.header('Access-Control-Allow-Credentials', 'true');
-  //     return res.status(200).json({});
-  //   }
-  //   next();
-  // });
 
   await app.listen(process.env.PORT ?? 3000);
 }
