1f11c284744919519c80238c73a4c1fde295d24c
JojoKsv <jkossouvi@gmail.com>
2025-01-07 14:39:30 +0000

    setting cors 4

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { Request, NextFunction, Response } from 'express';
-import * as express from '@nestjs/platform-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-const ENV = process.env.NODE_ENV;
-const front_URL = process.env.FRONTEND_URL;
+const ENV = process.env.NODE_ENV || 'DEV';
+const front_URL = process.env.FRONTEND_URL || 'https://localhost:5174';
 
 const origin = ENV === 'PROD' ? front_URL : 'https://localhost:5174';
-console.log(origin, express);
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new express.ExpressAdapter(),
-    { cors: true },
-  );
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(), {
+    cors: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -28,32 +25,17 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log('Request Headers:', req.headers);
-    next();
-  });
-
-  app.use(function (req: Request, res: Response, next: NextFunction) {
-    if (req.method === 'OPTIONS') {
-      res.header(
-        'Access-Control-Allow-Origin',
-        'https://genie-construction-eben-ezer.vercel.app',
-      );
-      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, Accept',
-      );
-      res.header('Access-Control-Allow-Credentials', 'true');
-      return res.status(200).json({});
-    }
-    next();
-  });
+  if (ENV === 'PROD') {
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      console.log('Request Headers:', req.headers);
+      next();
+    });
+  }
 
   app.enableCors({
-    origin: ['https://genie-construction-eben-ezer.vercel.app/'], // Frontend autorisé
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes autorisées
-    credentials: true, // Autoriser les cookies
+    origin: [origin],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
