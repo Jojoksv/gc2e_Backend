5bbc4615b1650c46eeb98ad28e58ab3a0ae65938
JojoKsv <jkossouvi@gmail.com>
2025-01-07 13:49:22 +0000

    setting cors 1

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     }),
   );
 
-  app.use(cookieParser());
+  // app.use(cookieParser());
 
   app.enableCors();
 
@@ -30,12 +30,12 @@ async function bootstrap() {
     next();
   });
 
-  // app.enableCors({
-  //   origin: ['https://genie-construction-eben-ezer.vercel.app'], // Frontend autorisé
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes autorisées
-  //   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // En-têtes autorisés
-  //   credentials: true, // Autoriser les cookies
-  // });
+  app.enableCors({
+    origin: ['https://genie-construction-eben-ezer.vercel.app'], // Frontend autorisé
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes autorisées
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // En-têtes autorisés
+    credentials: true, // Autoriser les cookies
+  });
 
   // app.use(function (req: Request, res: Response, next: NextFunction) {
   //   if (req.method === 'OPTIONS') {
