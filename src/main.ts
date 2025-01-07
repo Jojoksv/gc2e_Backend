1aebf9d2714453427db613cb22fffc9e720a820f
JojoKsv <jkossouvi@gmail.com>
2025-01-07 14:09:53 +0000

    setting cors 2

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { Request, NextFunction, Response } from 'express';
+import * as express from '@nestjs/platform-express';
 
 const ENV = process.env.NODE_ENV;
 const front_URL = process.env.FRONTEND_URL;
 
 const origin = ENV === 'PROD' ? front_URL : 'https://localhost:5174';
-console.log(origin);
+console.log(origin, express);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new express.ExpressAdapter(),
+    { cors: true },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,9 +26,9 @@ async function bootstrap() {
     }),
   );
 
-  // app.use(cookieParser());
+  app.use(cookieParser());
 
-  app.enableCors();
+  // app.enableCors();
 
   app.use((req: Request, res: Response, next: NextFunction) => {
     console.log('Request Headers:', req.headers);
