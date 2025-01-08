beac9bbe6d28dea5bf2a0b178485bc50d82372ff
JojoKsv <jkossouvi@gmail.com>
2025-01-08 11:43:09 +0000

    setting enable cors. Trying to set the cors error

--- src/main.ts
+++ src/main.ts
@@ -22,52 +22,11 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  if (ENV === 'PROD') {
-    app.use((req: Request, res: Response, next: NextFunction) => {
-      console.log('Request:', req);
-      next();
-    });
-  }
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://genie-construction-eben-ezer.vercel.app');
-    console.log('Res header:', res.header);
-    next();
-  });
-
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', 'https://genie-construction-eben-ezer.vercel.app');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      return res.status(204).end(); // Fin de la réponse pour OPTIONS
-    }
-    next();
-  });
-
-  var whitelist = ['https://genie-construction-eben-ezer.vercel.app'];
-
   app.enableCors({
-    origin: function (origin, callback) {
-      console.log('Origine de la requête:', origin);
-      if (origin === undefined) {
-        console.log('Aucune origine définie dans la requête.');
-      }
-
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('CORS autorisé pour:', origin);
-        callback(null, true);
-      } else {
-        console.log('CORS bloqué pour:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET, PUT, POST, DELETE, OPTIONS',
-    allowedHeaders: 'X-Requested-With, Content-Type, Accept, Observe',
+    origin: 'https://genie-construction-eben-ezer.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
-    preflightContinue: false,
-    maxAge: 86400,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
