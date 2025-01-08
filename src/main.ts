6108e25d4fa59ae7712cdf9a010b015a35dbe5bf
JojoKsv <jkossouvi@gmail.com>
2025-01-08 12:39:09 +0000

    setting enable cors. Trying to set the cors error Last setting with success

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { Request, Response, NextFunction } from 'express';
 const ENV = process.env.NODE_ENV || 'DEV';
 const front_URL = process.env.FRONTEND_URL || 'https://localhost:5174';
 
+const origin = ENV === 'PROD' ? front_URL : 'https://localhost:5174';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -24,46 +26,8 @@ async function bootstrap() {
 
   // CORS configuration
   app.enableCors({
-    origin: 'https://genie-construction-eben-ezer.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: origin,
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
-  // Middleware for setting headers
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://genie-construction-eben-ezer.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.method === 'OPTIONS') {
-      res.status(204).send(); // Répond avec 204 No Content
-    } else {
-      next(); // Continue vers d'autres middlewares
-    }
-  });
-
-  // Middleware for logging requests
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log(`[${req.method}] ${req.url}`);
-    next();
-  });
-
-  // Middleware pour gérer OPTIONS
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', 'https://genie-construction-eben-ezer.vercel.app');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      res.status(204).send(); // Réponse vide pour OPTIONS
-    } else {
-      next();
-    }
   });
 
   await app.listen(process.env.PORT ?? 3000);
