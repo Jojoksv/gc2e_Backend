0097e248ace13f93e1226e2fe5ac87855cf49fe8
JojoKsv <jkossouvi@gmail.com>
2025-01-08 15:22:59 +0000

    setting cookies errors 3

--- src/services/auth/auth.controller.ts
+++ src/services/auth/auth.controller.ts
@@ -38,7 +38,7 @@ export class AuthController {
       res.cookie('access_token', access_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'PROD',
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000,
       });
 
